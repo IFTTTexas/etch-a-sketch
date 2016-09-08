479313e68023b49c5ffad9015a57e241effe226f
bcook <brobrandon@icloud.com>
2016-09-08 17:43:27 -0500

    JS pathed and added. Experiencing problems with last function.

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+	$('.left').hover(function() {//Turn Button Red
+	$('.left').removeClass('.left_mouseover');
+		},function () {
+	$('.left').removeClass('.left_mouseover');
+	});
+});
---
+++ scripts/grid.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	//Button Lights up
+	$('.button').hover(function() {
+		$(this).toggleClass('mouseover');
+	});
+	
+
+	
+	$(".left").click(function(){
+	$(".Grid").empty();
+	squares = prompt("Pick a number 1-100. You can do more but it'll take forever.");
+	
+	var square_size = $('.Grid').height()/squares -2;
+	
+	for (i = 1; i <= squares; i++) {
+		for(j = 1; j <= squares; j ++){
+			$('.Grid').append('<div class="pixel"></div>');	
+			}
+			$('.Grid').append('<div class="line"></div>');
+			}
+			
+			$('.pixel').css('width',square_size);
+			$('.pixel').css('height',square_size);
+		
+	});
+
+	
+	//Idea to have it start out transparent and add opacity over time. Dud so far.
+	/*('.pixel').mouseenter(function() {	
+		
+		var sketchOpacity = $(this).css('opacity');
+		
+		if(sketchOpacity < 100){
+			$(this).css('opacity', sketchOpacity + 0.1);
+		}
+	});*/
+	('.pixel').mouseenter(function() {	
+			$(this).addClass('pixelLightUp');
+	});
+	
+});
